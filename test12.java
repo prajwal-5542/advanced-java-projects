5c70d464985b5b5b7b9dc0847e5ad36068117b08
prajwal-5542 <125641441+prajwal-5542@users.noreply.github.com>
2023-03-06 10:05:35 +0530

    Add files via upload

---
+++ test12.java
@@ -0,0 +1,32 @@
+package prajwal;
+class PrintTask implements Runnable{
+	public void run() {
+		try {
+			for(int i=5;i>=1;i--) {
+				System.out.println(Thread.currentThread()+" "+i);
+			}
+		}catch(Exception e) {}
+	}
+}
+
+public class test12 {
+
+	public static void main(String[] args) throws Exception {
+		PrintTask task=new PrintTask();
+		Thread t1=new Thread(task);
+		Thread t2=new Thread(task);
+		Thread t3=new Thread(task);
+		t1.start();
+		t2.start();
+		t3.start();
+		t1.setName("first");
+		t2.setName("second");
+		t3.setName("third");
+		t1.join();
+		t2.join();
+		t3.join();
+		System.out.println("main ends");
+		
+	}
+
+}
